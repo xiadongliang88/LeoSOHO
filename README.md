5946301f7a3391429b5a0c94737504fc32c05829
xiadongliang <xiadongliang88@163.com>
2020-02-12 15:48:46 +0800

    sssss

---
+++ README.md
@@ -0,0 +1,4 @@
+https://www.leosoho.cn
+
+Or download the main.py
+python main.py
---
+++ main.py
@@ -0,0 +1,8 @@
+import webbrowser
+
+def main():
+    url = 'https://www.leosoho.cn'
+    webbrowser.open_new_tab(url)
+
+if __name__ == '__main__':
+    main()
